c03a99b51fc1afaa3b57e6d59dc910f7f75f2348
Nathan Gaberel <nathang@theodo.co.uk>
2018-01-25 22:22:48 +0000

    Add react-router.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Route } from 'react-router-dom'
+import { ConnectedRouter } from 'react-router-redux'
 import { ThemeProvider } from 'styled-components'
 
 import App from './components/App'
+import history from './history'
 import store from './redux/store'
 import theme from './theme'
 
 ReactDOM.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <App />
+      <ConnectedRouter history={history}>
+        <Route component={App} />
+      </ConnectedRouter>
     </ThemeProvider>
   </Provider>,
   document.getElementById('app')
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 
 import api from './api'
 
 export default combineReducers({
-  api
+  api,
+  router: routerReducer
 })
