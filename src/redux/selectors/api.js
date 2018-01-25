f4d7b02a89b2c216ae18b539536df7d2cdf694af
Nathan Gaberel <nathang@theodo.co.uk>
2018-01-25 22:27:32 +0000

    Add example selector.

---
+++ src/redux/selectors/api.js
@@ -0,0 +1,8 @@
+import { createSelector } from 'reselect'
+
+const rawAPISelector = state => state.api
+
+export const isLoadingSelector = createSelector(
+  rawAPISelector,
+  api => api.loading
+)
