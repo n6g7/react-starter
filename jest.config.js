773716e9d695785aa3a9ee61200fa8af7123fad4
Nathan Gaberel <nathang@theodo.co.uk>
2018-01-26 18:11:19 +0000

    Fix test runner.

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  collectCoverageFrom: [
+    'src/**/*.js'
+  ],
+  coveragePathIgnorePatterns: [
+    'index.js$'
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 99,
+      branches: 99,
+      functions: 99,
+      lines: 99
+    }
+  },
+  setupFiles: [
+    './tests/setup.js'
+  ],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer'
+  ]
+}
